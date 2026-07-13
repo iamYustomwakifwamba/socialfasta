78450aa9fc42ccd5ac14f316b4a3f78379f7aab2
desploit <aloyceyusto2@gmail.com>
2026-07-13 09:53:44 +0300

    first commit

---
+++ socialfasta/sim_hub/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SimHubConfig(AppConfig):
+    name = 'sim_hub'
---
+++ socialfasta/smm_engine/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SmmEngineConfig(AppConfig):
+    name = 'smm_engine'
